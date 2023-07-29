af7f6be541b6bbff5809e7cf5de3cc09c122d58c
Joao Vitor Campos <vitorcampo0s640@gmail.com>
2023-07-29 10:05:56 -0300

    melhoria na estrutura de visualização das imagens dinamicas

--- st_main.py
+++ st_main.py
@@ -1,11 +1,9 @@
+from functions_img import brighten_img, threshold_img, contrast_img, sobel_img, canny_img, histogram_img, histogram_fn, channel_rgb, channel_ycbcr
+from functions_webcam import threshold_webcam, brighten_webcam, contrast_webcam, sobel_webcam, canny_webcam, channel_rgb_w, channel_ycbcr_w, gray_scale, img_webcam
 import streamlit as st
-import numpy as np
 from PIL import Image
-import cv2
+import numpy as np
 import cv2 as cv
-from functions_img import brighten_img, threshold_img, contrast_img, sobel_img, canny_img, histogram_img, histogram_fn
-from functions_webcam import threshold_webcam, brighten_webcam, contrast_webcam, sobel_webcam, canny_webcam
-from matplotlib import pyplot as plt
 
 
 st.set_page_config(layout="wide") #Deixando a página no modo wide por default
@@ -17,12 +15,16 @@ def main_loop():
     histogram = True
     bar = st.sidebar
     entry_option = bar.selectbox('Selecione o formato da entrada:', ['Upload da Imagem', 'Webcam'])
-    option = bar.selectbox('Selecione a técnica desejada:', ['Limiarização', 'Brilho', 'Contraste', 'Sobel', 'Canny'])
+    option = bar.selectbox('Selecione a técnica desejada:', ['Limiarização', 'Brilho', 'Contraste', 'Sobel', 'Canny', 'Visualizar'])
+    #scale = bar.radio('Selecione a escala:', ['Escala de Cinza', 'Escala de Cor'])
+
+    bar.markdown(''' ### Opções: ''')
+    vis = img2 = img3 = img4 = text_frame3 = text_frame4 = ''
 
     match entry_option:
 
         case 'Upload da Imagem':
-            image = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
+            image = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg','tif'])
 
             if not image: 
                 return None
@@ -30,65 +32,158 @@ def main_loop():
             else: 
                 original_image = Image.open(image)
                 original_image = np.array(original_image)
+                img = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
 
             match option:
 
                 case 'Limiarização':
-                    histogram = False
                     threshold_value = bar.slider("Limiarização", min_value=-1, max_value=255, value=-1)
-                    processed_image = threshold_img(original_image, threshold_value)
+                    text_frame2 = 'Imagem binarizada'
+                    img2 = threshold_img(img, threshold_value)
 
                 case 'Brilho':
-                    histogram = True
                     brightness_value = bar.slider("Brilho", min_value=-1, max_value=255, value=0)
-                    processed_image = brighten_img(original_image, brightness_value)
+                    text_frame2 = 'Imagem de saída'
+                    text_frame3 = 'Histograma da imagem de entrada'
+                    text_frame4 = 'Histograma da imagem de saída'
+                    img2 = brighten_img(img, brightness_value)
 
                 case 'Contraste':
-                    histogram = True
-                    contrast_value = bar.slider("Contraste", min_value=-1, max_value=100, value=0)
-                    processed_image = contrast_img(original_image, contrast_value)   
+                    contrast_value = bar.slider("Contraste", min_value=0.0, max_value=2.0, value=0.0)
+                    text_frame2 = 'Imagem de saída'
+                    text_frame3 = 'Histograma da imagem de entrada'
+                    text_frame4 = 'Histograma da imagem de saída'
+                    img2 = contrast_img(img, contrast_value)
 
                 case 'Sobel':
-                    histogram = False
                     sobel_value_1 = bar.slider("Sobel X", min_value=1, max_value=4, value=1)
                     sobel_value_2 = bar.slider("Sobel Y", min_value=1, max_value=4, value=1)
-                    processed_image = sobel_img(original_image, sobel_value_1, sobel_value_2)   
+                    text_frame2 = 'Imagem de saída'
+                    img2 = sobel_img(img, sobel_value_1, sobel_value_2)   
 
                 case 'Canny':
-                    histogram = False
-                    canny_value_1 = bar.slider("Canny dsad", min_value=0, max_value=255, value=0)
-                    canny_value_2 = bar.slider("Canny asdas", min_value=0, max_value=255, value=0)
-                    processed_image = canny_img(original_image, canny_value_1, canny_value_2)
+                    canny_value_1 = bar.slider("Limiar mínimo", min_value=0, max_value=255, value=0)
+                    canny_value_2 = bar.slider("Limiar máximo", min_value=0, max_value=255, value=0)
+                    text_frame2 = 'Imagem de saída'
+                    img2 = canny_img(img, canny_value_1, canny_value_2)
+
+                case 'Visualizar':
+
+                    vis = bar.selectbox('Selecione o formato da conversão:', ['Escala de Cinza','RGB', 'YCBCR'])
+                    img = original_image
+
+                    match vis:
+
+                        case 'Escala de Cinza':
+                            text_frame2 = 'Imagem em escala de cinza' 
+                            img2 = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+                        case 'RGB':
+                            text_frame2 = "Componente Vermelha (R)"
+                            img2 = channel_rgb(original_image, 'r')
+                            text_frame3 = "Componente Verde (G)"
+                            img3 = channel_rgb(original_image, 'g')
+                            text_frame4 = "Componente Verde (B)"
+                            img4 = channel_rgb(original_image, 'b')
+
+                        case 'YCBCR':
+                            text_frame2 = "Componente Y"
+                            img2 = channel_ycbcr(original_image, 'y')
+                            text_frame3 = "Componente Cb"
+                            img3 = channel_ycbcr(original_image, 'cb')
+                            text_frame4 = "Componente Cr"
+                            img4 = channel_ycbcr(original_image, 'cr')
+
+            col1, col2 = st.columns(2)
+            col3, col4 = st.columns(2)
+
+            with col1: # Coluna da img de entrada
+                st.text("Imagem de entrada")
+                st.image([img], width=550)
+
+            with col2: # Coluna da img de entrada
+                st.text(f'{text_frame2}')
+                st.image([img2], width=550)
+
+            with col3: # Coluna da img de entrada
+                st.text(f'{text_frame3}')
+
+                match option:
+
+                    case 'Brilho' | 'Contraste':
+                        histogram_fn(img)
+
+                match vis:
+                    
+                    case 'RGB' | 'YCBCR':
+                        st.image([img3], width=550)
+                    
+
+            with col4: # Coluna da img de entrada
+                
+                st.text(f'{text_frame4}')
+
+                match option:
+
+                    case 'Brilho' | 'Contraste':
+                        histogram_fn(img2)
+
+                match vis:
+                    
+                    case 'RGB' | 'YCBCR':
+                        st.image([img4], width=550)
 
         case 'Webcam':
-            image = ''
-            st.text('Imagem de entrada                                                Imagem de saída')
-            FRAME_WINDOW  = st.image([])
-            camera = cv2.VideoCapture(0)
+
+            image = vis = None
+            camera = cv.VideoCapture(0)
+
+            text_frame2 = text_frame3 = text_frame4 = ''
+
+            #Estrutura da imagem dinâmica
             col1, col2 = st.columns(2)
-            
+            col3, col4 = st.columns(2)
+
+            with col1:
+                st.text("Imagem de entrada")
+                frame = st.image([])
+            with col2:
+                text_frame2 = st.text("")
+                frame2 = st.image([])
+            with col3:
+                text_frame3 = st.text("")
+                frame3 = st.image([])
+            with col4:
+                text_frame4 = st.text("")
+                frame4 = st.image([])
+
             match option:
 
                 case 'Limiarização':
                     histogram = False
                     threshold_value = bar.slider("Limiarização", min_value=-1, max_value=255, value=-1)
-
                     while 'True' == 'True':
-                        threshold_webcam(camera, FRAME_WINDOW, threshold_value)
+                        img_webcam(camera, frame, 'gray')
+                        text_frame2.text("Imagem binarizada")
+                        threshold_webcam(camera, frame2, threshold_value)
 
                 case 'Brilho':
                     histogram = False
                     brightness_value = bar.slider("Brilho", min_value=0, max_value=255, value=0)
                     
                     while 'True' == 'True':
-                        brighten_webcam(camera, FRAME_WINDOW, brightness_value)
+                        img_webcam(camera, frame, 'gray')
+                        text_frame2.text("Imagem Saída")
+                        brighten_webcam(camera, frame2, brightness_value)
 
                 case 'Contraste':
                     histogram = False
-                    contrast_value = bar.slider("Contraste", min_value=0, max_value=100, value=0)
+                    contrast_value = bar.slider("Contraste", min_value=0, max_value=200, value=0)
 
                     while 'True' == 'True':
-                        contrast_webcam(camera, FRAME_WINDOW, contrast_value)
+                        img_webcam(camera, frame, 'gray')
+                        text_frame2.text("Imagem Saída")
+                        contrast_webcam(camera, frame2, contrast_value)
 
                 case 'Sobel':
                     histogram = False
@@ -96,51 +191,51 @@ def main_loop():
                     sobel_value_2 = bar.slider("Sobel Y", min_value=1, max_value=4, value=1)
                     
                     while 'True' == 'True':
-                        sobel_webcam(camera, FRAME_WINDOW, sobel_value_1, sobel_value_2)
+                        img_webcam(camera, frame, 'gray')
+                        text_frame2.text("Imagem Saída")
+                        sobel_webcam(camera, frame2, sobel_value_1, sobel_value_2)
 
                 case 'Canny':
                     histogram = False
-                    canny_value_1 = bar.slider("Canny dsad", min_value=0, max_value=255, value=0)
-                    canny_value_2 = bar.slider("Canny asdas", min_value=0, max_value=255, value=0)
+                    canny_value_1 = bar.slider("Limiar mínimo", min_value=0, max_value=255, value=0)
+                    canny_value_2 = bar.slider("Limiar máximo", min_value=0, max_value=255, value=0)
                     
                     while 'True' == 'True':
-                        canny_webcam(camera, FRAME_WINDOW, canny_value_1, canny_value_2)
-    
-    #Estrutura da Ferramenta
+                        img_webcam(camera, frame, 'gray')
+                        text_frame2.text("Imagem Saída")
+                        canny_webcam(camera, frame2, canny_value_1, canny_value_2)
 
-    # Campo de imagens de entrada e Saída
-    col_1, col_2 = st.columns(2)
+                case 'Visualizar':
+                    histogram = processed_image = None
+                    vis = bar.selectbox('Selecione o formato de visualização:', ['Escala de Cinza','RGB', 'YCBCR'])
 
-    match entry_option:
+                    match vis:
 
-        case 'Upload da Imagem':
+                        case 'Escala de Cinza':
+                            while 'True' == 'True':
+                                img_webcam(camera, frame, 'color')
+                                text_frame2.text("Imagem em escala de cinza")
+                                gray_scale(camera, frame2)
 
-            with col_1: # Coluna da img de entrada
-                st.text("Imagem de entrada")
-                st.image([original_image], width=500)
+                        case 'RGB':
+                            while 'True' == 'True':
+                                img_webcam(camera, frame, 'color')
+                                text_frame2.text("Componente Vermelha (R)")
+                                channel_rgb_w(camera, frame2, 'r')
+                                text_frame3.text("Componente Verde (G)")
+                                channel_rgb_w(camera, frame3, 'g')
+                                text_frame4.text("Componente Verde (B)")
+                                channel_rgb_w(camera, frame4, 'b')
 
-            with col_2: #Coluna da img de saída
-                st.text("Imagem de saída")
-                st.image([processed_image], width=500)
-
-    #Campo para Histogramas de entrada e Saída
-    st.text("")
-    histcol1, histcol2 = st.columns(2)
-    fig = plt.figure(figsize=(8,8))
-
-    match option:
-
-        case 'Brilho' | 'Contraste':
-
-            with histcol1: #Coluna hist de entrada
-                st.text("Histograma imagem de entrada")
-                if histogram == True:
-                    histogram_fn(original_image)
-
-            with histcol2: #Coluna hist de saída
-                st.text("Histograma imagem de saída")
-                if histogram == True:
-                    histogram_fn(processed_image)
+                        case 'YCBCR':
+                            while 'True' == 'True':
+                                img_webcam(camera, frame, 'color')
+                                text_frame2.text("Componente Y")
+                                channel_ycbcr_w(camera, frame2, 'y')
+                                text_frame3.text("Componente Cb")
+                                channel_ycbcr_w(camera, frame3, 'cb')
+                                text_frame4.text("Componente Cr")
+                                channel_ycbcr_w(camera, frame4, 'cr')
 
 if __name__ == '__main__':
     main_loop()
